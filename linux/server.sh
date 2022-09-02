16da9dc3c363a4e9d16282b7d8b9ed103823d61b
root <root@ip-172-31-89-88.ec2.internal>
2022-09-02 05:47:26 +0000

    update2

--- linux/server.sh
+++ linux/server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "hello"
-echo "hostname $(hostname) "
-echo "cpu details $(lscpu) "
-echo "mem details $(lsmem) "
-echo "block details $(lsblk) "
-echo "name is $(uname -a) "
+echo "seever name is : ${hostname} "
+echo "cpu details :${lscpu} "
+echo "mem details :${lsmem} "
+echo "block details: ${lsblk} "
+echo "name is : ${uname -a} "
